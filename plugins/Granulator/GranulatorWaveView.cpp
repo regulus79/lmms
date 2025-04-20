ac06be518e1727c1153e648a4c6df211a012e798
regulus79 <117475203+regulus79@users.noreply.github.com>
2025-04-20 16:13:02 -0400

    Working again

--- plugins/Granulator/GranulatorWaveView.cpp
+++ plugins/Granulator/GranulatorWaveView.cpp
@@ -24,9 +24,10 @@
 
 #include "GranulatorWaveView.h"
 
+#include "Sample.h"
 #include "ConfigManager.h"
+#include "SampleThumbnail.h"
 #include "FontHelper.h"
-#include "SampleWaveform.h"
 
 #include <QPainter>
 #include <QMouseEvent>
@@ -83,7 +84,8 @@ GranulatorWaveView::GranulatorWaveView(QWidget* parent, int w, int h, Sample con
 	m_isDragging(false),
 	m_reversed(false),
 	m_framesPlayed(0),
-	m_animation(ConfigManager::inst()->value("ui", "animateafp").toInt())
+	m_animation(ConfigManager::inst()->value("ui", "animateafp").toInt()),
+	m_sampleThumbnail(*buf)
 {
 	setFixedSize(w, h);
 	setMouseTracking(true);
@@ -168,16 +170,22 @@ void GranulatorWaveView::mouseMoveEvent(QMouseEvent * me)
 		case DraggingType::SampleLoop:
 			slideSamplePointByPx(Point::Loop, step);
 			break;
+		case DraggingType::SlideWave:
+			slide(step);
+			break;
+		case DraggingType::ZoomWave:
+			zoom(me->y() < m_draggingLastPoint.y());
+			break;
 		case DraggingType::Wave:
 		default:
 			if (qAbs(me->y() - m_draggingLastPoint.y())
 				< 2 * qAbs(me->x() - m_draggingLastPoint.x()))
 			{
-				slide(step);
+				m_draggingType = DraggingType::SlideWave;
 			}
 			else
 			{
-				zoom(me->y() < m_draggingLastPoint.y());
+				m_draggingType = DraggingType::ZoomWave;
 			}
 	}
 
@@ -335,39 +343,47 @@ void GranulatorWaveView::updateGraph()
 	QPainter p(&m_graph);
 	p.setPen(QColor(255, 255, 255));
 
-	const auto rect = QRect{0, 0, m_graph.width(), m_graph.height()};
-	const auto waveform = SampleWaveform::Parameters{
-		m_sample->data() + m_from, static_cast<size_t>(range()), m_sample->amplification(), m_sample->reversed()};
-	SampleWaveform::visualize(waveform, p, rect);
+	m_sampleThumbnail = SampleThumbnail{*m_sample};
+
+	const auto param = SampleThumbnail::VisualizeParameters{
+		.sampleRect = m_graph.rect(),
+		.amplification = m_sample->amplification(),
+		.sampleStart = static_cast<float>(m_from) / m_sample->sampleSize(),
+		.sampleEnd = static_cast<float>(m_to) / m_sample->sampleSize(),
+		.reversed = m_sample->reversed(),
+	};
+
+	m_sampleThumbnail.visualize(param, p);
 }
 
 void GranulatorWaveView::zoom(const bool out)
 {
-	const f_cnt_t start = m_sample->startFrame();
-	const f_cnt_t end = m_sample->endFrame();
-	const f_cnt_t frames = m_sample->sampleSize();
-	const f_cnt_t d_from = start - m_from;
-	const f_cnt_t d_to = m_to - end;
+	const auto start = m_sample->startFrame();
+	const auto end = m_sample->endFrame();
+	const auto frames = m_sample->sampleSize();
 
-	const f_cnt_t step = qMax(1.0, qMax(d_from, d_to) / 10.0);
-	const f_cnt_t step_from = (out ? - step : step);
-	const f_cnt_t step_to = (out ? step : - step);
+	const auto dFrom = start - m_from;
+	const auto dTo = m_to - end;
 
-	const double comp_ratio = double(qMin(d_from, d_to))
-								/ qMax(1, static_cast<int>(qMax(d_from, d_to)));
+	const auto step = std::max(1.0, std::max(dFrom, dTo) / 10.0);
+	const auto stepFrom = out ? -step : step;
+	const auto stepTo = out ? step : -step;
 
-	const auto boundedFrom = std::clamp(static_cast<double>(m_from + step_from), 0.0, static_cast<double>(start));
-	const auto boundedTo = std::clamp(static_cast<double>(m_to + step_to), static_cast<double>(end), static_cast<double>(frames));
+	const auto boundedFrom = std::clamp(m_from + stepFrom, 0.0, static_cast<double>(start));
+	const auto boundedTo = std::clamp(m_to + stepTo, static_cast<double>(end), static_cast<double>(frames));
 
-	const auto toStep = static_cast<f_cnt_t>(step_from * (boundedTo == m_to ? 1 : comp_ratio));
-	const auto newFrom
-		= (out && d_from < d_to) || (!out && d_to < d_from) ? boundedFrom : std::clamp(static_cast<int>(m_from + toStep), 0, static_cast<int>(start));
+	const auto compRatio = std::min(dFrom, dTo) / static_cast<double>(std::max(1, std::max(dFrom, dTo)));
+	const auto toStep = stepFrom * (boundedTo == m_to ? 1 : compRatio);
+	const auto newFrom = (out && dFrom < dTo) || (!out && dTo < dFrom)
+		? boundedFrom
+		: std::clamp(m_from + toStep, 0.0, static_cast<double>(start));
 
-	const auto fromStep = static_cast<f_cnt_t>(step_to * (boundedFrom == m_from ? 1 : comp_ratio));
-	const auto newTo
-		= (out && d_from < d_to) || (!out && d_to < d_from) ? std::clamp(m_to + fromStep, end, frames) : boundedTo;
+	const auto fromStep = stepTo * (boundedFrom == m_from ? 1 : compRatio);
+	const auto newTo = (out && dFrom < dTo) || (!out && dTo < dFrom)
+		? std::clamp(m_to + fromStep, static_cast<double>(end), static_cast<double>(frames))
+		: boundedTo;
 
-	if (static_cast<double>(newTo - newFrom) / m_sample->sampleRate() > 0.05)
+	if ((newTo - newFrom) / m_sample->sampleRate() > 0.05)
 	{
 		setFrom(newFrom);
 		setTo(newTo);
@@ -377,19 +393,15 @@ void GranulatorWaveView::zoom(const bool out)
 void GranulatorWaveView::slide(int px)
 {
 	const double fact = qAbs(double(px) / width());
-	f_cnt_t step = range() * fact;
-	if (px > 0)
-	{
-		step = -step;
-	}
+	auto step = range() * fact * (px > 0 ? 1 : -1);
 
-	f_cnt_t step_from = qBound<size_t>(0, m_from + step, m_sample->sampleSize()) - m_from;
-	f_cnt_t step_to = qBound<size_t>(m_from + 1, m_to + step, m_sample->sampleSize()) - m_to;
+	const auto sampleStart = static_cast<double>(m_sample->startFrame());
+	const auto sampleEnd = static_cast<double>(m_sample->endFrame());
 
-	step = qAbs(step_from) < qAbs(step_to) ? step_from : step_to;
+	const auto stepFrom = std::clamp(sampleStart + step, 0.0, static_cast<double>(m_sample->sampleSize())) - sampleStart;
+	const auto stepTo = std::clamp(sampleEnd + step, sampleStart + 1.0, static_cast<double>(m_sample->sampleSize())) - sampleEnd;
+	step = std::abs(stepFrom) < std::abs(stepTo) ? stepFrom : stepTo;
 
-	setFrom(m_from + step);
-	setTo(m_to + step);
 	slideSampleByFrames(step);
 }
 
@@ -401,7 +413,7 @@ void GranulatorWaveView::slideSamplePointByPx(Point point, int px)
 	);
 }
 
-void GranulatorWaveView::slideSamplePointByFrames(Point point, f_cnt_t frames, bool slide_to)
+void GranulatorWaveView::slideSamplePointByFrames(Point point, long frameOffset, bool slideTo)
 {
 	knob * a_knob = m_startKnob;
 	switch(point)
@@ -421,8 +433,8 @@ void GranulatorWaveView::slideSamplePointByFrames(Point point, f_cnt_t frames, b
 	}
 	else
 	{
-		const double v = static_cast<double>(frames) / m_sample->sampleSize();
-		if (slide_to)
+		const double v = static_cast<double>(frameOffset) / m_sample->sampleSize();
+		if (slideTo)
 		{
 			a_knob->slideTo(v);
 		}
@@ -436,13 +448,13 @@ void GranulatorWaveView::slideSamplePointByFrames(Point point, f_cnt_t frames, b
 
 
 
-void GranulatorWaveView::slideSampleByFrames(f_cnt_t frames)
+void GranulatorWaveView::slideSampleByFrames(long frameOffset)
 {
 	if (m_sample->sampleSize() <= 1)
 	{
 		return;
 	}
-	const double v = static_cast<double>(frames) / m_sample->sampleSize();
+	const double v = static_cast<double>(frameOffset) / m_sample->sampleSize();
 	// update knobs in the right order
 	// to avoid them clamping each other
 	if (v < 0)
@@ -466,11 +478,11 @@ void GranulatorWaveView::reverse()
 			- m_sample->endFrame()
 			- m_sample->startFrame()
 	);
+	
+	const int fromTmp = m_from;
 
-	const f_cnt_t from = m_from;
 	setFrom(m_sample->sampleSize() - m_to);
-	setTo(m_sample->sampleSize() - from);
-
+	setTo(m_sample->sampleSize() - fromTmp);
 	m_reversed = ! m_reversed;
 }
 
--- plugins/Granulator/GranulatorWaveView.h
+++ plugins/Granulator/GranulatorWaveView.h
@@ -27,6 +27,7 @@
 
 
 #include "Knob.h"
+#include "SampleThumbnail.h"
 
 
 namespace lmms
@@ -118,6 +119,8 @@ private:
 	enum class DraggingType
 	{
 		Wave,
+		SlideWave,
+		ZoomWave,
 		SampleStart,
 		SampleEnd,
 		SampleLoop
@@ -125,23 +128,24 @@ private:
 
 	Sample const* m_sample;
 	QPixmap m_graph;
-	f_cnt_t m_from;
-	f_cnt_t m_to;
-	f_cnt_t m_last_from;
-	f_cnt_t m_last_to;
+	int m_from;
+	int m_to;
+	int m_last_from;
+	int m_last_to;
 	float m_last_amp;
 	knob* m_startKnob;
 	knob* m_endKnob;
 	knob* m_loopKnob;
-	f_cnt_t m_startFrameX;
-	f_cnt_t m_endFrameX;
-	f_cnt_t m_loopFrameX;
+	int m_startFrameX;
+	int m_endFrameX;
+	int m_loopFrameX;
 	bool m_isDragging;
 	QPoint m_draggingLastPoint;
 	DraggingType m_draggingType;
 	bool m_reversed;
 	f_cnt_t m_framesPlayed;
 	bool m_animation;
+	SampleThumbnail m_sampleThumbnail;
 
 	friend class GranulatorView;
 
@@ -158,8 +162,8 @@ private:
 	void zoom(const bool out = false);
 	void slide(int px);
 	void slideSamplePointByPx(Point point, int px);
-	void slideSamplePointByFrames(Point point, f_cnt_t frames, bool slide_to = false);
-	void slideSampleByFrames(f_cnt_t frames);
+	void slideSamplePointByFrames(Point point, long frameOffset, bool slideTo = false);
+	void slideSampleByFrames(long frameOffset);
 
 	void slideSamplePointToFrames(Point point, f_cnt_t frames)
 	{
